27b55342e74b37aedb535939df4a9260817b10ee
João <joao.nascimento@rntecnologia.com.br>
2023-12-19 18:29:09 -0300

    Login e autorização

--- js/index.js
+++ js/index.js
@@ -1,34 +1,66 @@
 import tarefaController from "../js/controller/TarefaController.js";
+import loginController from "../js/controller/LoginController.js";
 
 // Função auxiliar para selecionar elementos no DOM.
 const $ = document.querySelector.bind(document);
 const componentePrincipal = $("#conteudo_principal");
 
-// Renderiza a lista de tarefas como visualização inicial.
-tarefaController.renderizarListaTarefas(componentePrincipal);
+/**
+ * Verifica se o usuário está logado. 
+ * Mostra ou esconde o conteúdo principal com base no status de login.
+ */
+function verificarLogin() {
+  const token = localStorage.getItem('token');
+  if (token) {
+    mostrarConteudoPrincipal();
+  } else {
+    esconderConteudoPrincipal();
+  }
+}
+
+/**
+ * Mostra o conteúdo principal e esconde a tela de login.
+ */
+function mostrarConteudoPrincipal() {
+  $('#login').style.display = 'none';
+  $('#app').style.display = 'block';
+}
+
+/**
+ * Esconde o conteúdo principal, remove o token do localStorage e mostra a tela de login.
+ */
+function esconderConteudoPrincipal() {
+  localStorage.removeItem('token');
+  $('#login').style.display = 'flex';
+  $('#app').style.display = 'none';
+  $("#usuario").value = "";
+  $("#senha").value = "";
+}
+
+// Adiciona um ouvinte de evento para verificar o login quando o documento estiver totalmente carregado.
+document.addEventListener('DOMContentLoaded', verificarLogin);
+
+// Adiciona ouvintes de eventos para itens de menu.
+$("#tarefa").addEventListener("click", function() {
+  fecharNavBar();
+  tarefaController.renderizarTarefaFormulario(componentePrincipal);
+});
+
+$("#lista_tarefas").addEventListener("click", function() {
+  fecharNavBar();
+  tarefaController.renderizarListaTarefas(componentePrincipal);
+});
+
+// Adiciona ouvinte de evento para o formulário de login.
+$("#login-form").addEventListener('submit', loginController.realizarLogin);
+
+// Adiciona ouvinte de evento para o botão de logout.
+$("#logout").addEventListener("click", esconderConteudoPrincipal);
 
 /**
  * Fecha a barra de navegação lateral (navbar).
  */
 function fecharNavBar() {
-  const closeCanvas = document.querySelector('[data-bs-dismiss="offcanvas"]');
+  const closeCanvas = $('[data-bs-dismiss="offcanvas"]');
   closeCanvas.click();
 }
-
-/**
- * Configura o evento de clique para o item de menu 'Tarefa'.
- * Fecha a navbar e renderiza o formulário de tarefa.
- */
-$("#tarefa").addEventListener("click", () => {
-  fecharNavBar();
-  tarefaController.renderizarTarefaFormulario(componentePrincipal);
-});
-
-/**
- * Configura o evento de clique para o item de menu 'Lista de Tarefas'.
- * Fecha a navbar e renderiza a lista de tarefas.
- */
-$("#lista_tarefas").addEventListener("click", () => {
-  fecharNavBar();
-  tarefaController.renderizarListaTarefas(componentePrincipal);
-});
